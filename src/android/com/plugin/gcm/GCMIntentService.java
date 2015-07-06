0742f29be4999244d74a11da8b62ed05099fef09
Gabriel Prado <gabriel.prado@coffeebeantech.com>
2015-07-06 18:16:46 -0300

    Avoid public exported activity
    See:
    https://code.google.com/p/android/issues/detail?id=61850
    https://code.google.com/p/android/issues/detail?id=63236
    http://stackoverflow.com/questions/21250364/notification-click-not-launch-the-given-activity-on-nexus-phones

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -88,7 +88,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 		notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		notificationIntent.putExtra("pushBundle", extras);
 
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 		
 		int defaults = Notification.DEFAULT_ALL;
 
